18adf1de8b4ec6992ee904aed56224e971821e2c
navidnazem <nazem.navid@gmail.com>
2025-03-08 23:18:40 +0100

    debug print

--- src/api/api.js
+++ src/api/api.js
@@ -133,6 +133,7 @@ async function getUserCoins(userId) {
 
     try {
         const response = await fetch(url);
+        console.log("response", response);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
